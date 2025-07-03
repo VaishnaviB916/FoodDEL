8e9a8fa245e678c986453fd2abd73a02d57f730f
VaishnaviB916 <vaishnavibotcha@gmail.com>
2025-07-03 13:37:53 +0530

    Initial commit - full FoodDEL MERN project

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Discover a world of flavor on every plate — fresh, flavorful, and made just for you.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
